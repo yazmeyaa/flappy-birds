3848be57a5ec3c5c0a6ecee55be54afc3f880fa5
Eugene <evgenijantonenkov456@gmail.com>
2024-07-24 19:28:40 +0300

    Add path aliases

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+const root = resolve(__dirname, "src");
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@/": resolve(root),
+      "@/engine": resolve(root, "game", "engine"),
+      "@/entities": resolve(root, "game", "entities"),
+      "@/components": resolve(root, "game", "components"),
+      "@/systems": resolve(root, "game", "systems"),
+      "@/assets": resolve(root, "game", "assets"),
+    },
+  },
+});
